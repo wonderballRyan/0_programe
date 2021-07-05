829d693ff8f652049f49f72caca0e05c459609d1
Ryan <34503835+wonderballRyan@users.noreply.github.com>
2021-07-05 22:21:05 +0800

    Create 3_happyNum.cpp
    3、快乐数判断

---
+++ C++/3_hashTable/3_happyNum.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	//取数值各个位上的单数之和
+	int getSum(int n) {
+		int sum = 0;
+		while (n) {
+			sum += (n % 10) * (n % 10);
+			n /= 10;
+		}
+		return sum;
+	}
+
+	bool isHappy(int n) {
+		unordered_set<int> set;
+		while (1) {
+			int sum = getSum(n);
+			if (sum == 1)
+			{
+				return true;
+			}
+
+			//如果这个sum曾经出现过，说明已经陷入了无限循环，立刻return false
+			if (set.find(sum) != set.end()) {
+				return false;
+			}
+			else {
+				set.insert(sum);
+			}
+			n = sum;
+		}
+
+	}
+};
+int main() {
+	Solution s;
+	cout << s.isHappy(19) << endl;
+}
