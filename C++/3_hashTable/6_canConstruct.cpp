cd78aa48edfec51b38867ce98e50786f3916f0fb
Ryan <34503835+wonderballRyan@users.noreply.github.com>
2021-07-06 22:47:00 +0800

    Create 6_canConstruct.cpp
    6、判断一个字符串中的字母是否能组成另一个字符串，要求不允许重复使用，且没有大写字母

---
+++ C++/3_hashTable/6_canConstruct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool canConstruct(string ransomNote, string magazine) {
+		int record[26] = { 0 };
+		for (int i = 0; i < magazine.length(); i++) {
+			//通过record数据记录，magazine里各个字符出现次数
+			record[magazine[i] - 'a']++;
+		}
+
+		for (int j = 0; j < ransomNote.length(); j++) {
+			//遍历ransomNote，在record里对应的字符个数做--操作
+			record[ransomNote[j] - 'a']--;
+			//如果小于零说明ransomNote里出现的字符，magazine没有
+			if (record[ransomNote[j] - 'a'] < 0) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+};
+
+int main() {
+	string a ="num";
+	string b = "number";
+	cout << Solution().canConstruct(a, b) << endl;
+}
